52fade584a8f8ee026a13ac944a887651db1909c
lela0 <lela0@192.168.1.202>
2022-12-24 17:29:43 -0500

    Added stuff

--- Flights/src/controllers/LoginController.java
+++ Flights/src/controllers/LoginController.java
@@ -43,7 +43,7 @@ public class LoginController{
 	}
 	
 	public void goToAdminLogin (ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("../gui/AdminLogin.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("../gui/Login.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
