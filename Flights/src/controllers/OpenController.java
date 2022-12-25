79067eedcbd8de8065b340aa8ed15fcb89baabe0
lela0 <lela0@192.168.1.202>
2022-12-25 18:41:54 -0500

    set up actions and fix some scenes

--- Flights/src/controllers/OpenController.java
+++ Flights/src/controllers/OpenController.java
@@ -12,18 +12,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class SplashController {
+public class OpenController {
 	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
-	
-	public void start (ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("../gui/Login.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	public void start(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("../gui/Login.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
 	}
+	
 }
