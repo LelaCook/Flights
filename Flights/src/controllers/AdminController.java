28f6364881fc55d84fe96651b0d83040af22806a
LelaCook <114542242+LelaCook@users.noreply.github.com>
2022-12-25 18:53:03 -0500

    Update AdminController.java

--- Flights/src/controllers/AdminController.java
+++ Flights/src/controllers/AdminController.java
@@ -6,13 +6,27 @@ import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class AdminController {
 	
 	@FXML
 	private Button addButton;
+	private Button deleteButton;
+	private TextField flightId;
+	private TextField capacity;
+	private TextField cityFrom;
+	private TextField cityto;
+	private TextField idToDelete;
+	private DatePicker depart;
+	private DatePicker arrive;
 	
-	public void add (ActionEvent event) throws IOException, SQLException, ClassNotFoundException  {
+	public void add (ActionEvent event) throws IOException, SQLException  {
+		
+	}
+	
+	public void delete (ActionEvent event) throws IOException, SQLException {
 		
 	}
 
