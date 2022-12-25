bd1094fd9e28b2096876dabfabb133b94aede126
lela0 <lela0@192.168.1.202>
2022-12-25 09:50:27 -0500

    added scenes(dont push classpath)

--- Flights/src/application/Main.java
+++ Flights/src/application/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,15 +14,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-    	//added ../gui
+    	
 		Parent root = FXMLLoader.load(getClass().getResource("../gui/Open.fxml"));
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
+		
     } 
 
-	}
+}
 	
 
