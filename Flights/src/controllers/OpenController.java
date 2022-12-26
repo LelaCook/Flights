e98f9c3cf5e75513384a791cba79b00e4d1c8ff8
LelaCook <114542242+LelaCook@users.noreply.github.com>
2022-12-25 19:04:01 -0500

    Update OpenController.java

--- Flights/src/controllers/OpenController.java
+++ Flights/src/controllers/OpenController.java
@@ -18,12 +18,12 @@ public class OpenController {
 	private Scene scene;
 	private Parent root;
 	
-	
-	public void start (ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("../gui/Login.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	public void start(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("../gui/Login.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
 	}
+	
 }
