2a053947b82324688ddaec4282984bf0c2158c84
lela0 <lela0@192.168.1.202>
2022-12-25 19:34:50 -0500

    added gotoadmin(not ready yet)

--- Flights/src/controllers/LoginController.java
+++ Flights/src/controllers/LoginController.java
@@ -26,13 +26,19 @@ public class LoginController{
 	
 	@FXML
 	private Label label_login_message;
-	@FXML
 	private TextField tf_username;
-	@FXML
 	private TextField tf_password;
 	
-	public void register (ActionEvent event) throws IOException {
+	public void register(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("../gui/Register.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void admin(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../gui/AdminLogin.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
