8a8608d5480fe2ec0e1a6715d4e6f620e0b468c9
aarons <aarons@aaronss-air.attlocal.net>
2022-12-27 13:41:43 -0500

    Added label message when flight is added, fixed dates

--- Flights/src/controllers/AdminController.java
+++ Flights/src/controllers/AdminController.java
@@ -15,6 +15,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -31,6 +33,14 @@ public class AdminController {
 	private TextField tf_fromcity;
 	@FXML
 	private TextField tf_id_delete;
+	@FXML
+	private TextField dp_depart;
+	@FXML
+	private TextField dp_arrive;
+	@FXML
+	private Label label_add_message;
+	@FXML
+	private Label label_delete_message;
 
 
 	private Stage stage;
@@ -42,7 +52,7 @@ public class AdminController {
 					("jdbc:sqlserver://javaflightdb.database.windows.net:1433;database=javaflightdb;user=javaflightdb@javaflightdb;password=CISproject22!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 			
 			try {
-			String b = "INSERT INTO Flightts (flightid, capacity, tocity, fromcity) VALUES (?, ?, ?, ?)";
+			String b = "INSERT INTO FLIGHTS3 (flightid, capacity, tocity, fromcity, departdate, arrivedate) VALUES (?, ?, ?, ?, ?, ?)";
 			PreparedStatement statement = connection.prepareStatement(b);
 		
 			//wont work when i try to get tf from user (look below)
@@ -51,12 +61,13 @@ public class AdminController {
 			statement.setString(2, tf_capacity.getText());
 			statement.setString(3, tf_tocity.getText());
 			statement.setString(4, tf_fromcity.getText());
-	
-			System.out.println("Inserted");
-			
-			statement.executeUpdate(); 
-			
+			statement.setString(5, dp_depart.getText());
+			statement.setString(6, dp_arrive.getText());
 
+			statement.executeUpdate(); 
+			System.out.println("Inserted");
+			label_add_message.setText("Flight added!");
+			
 			}catch (Exception e) {
 				e.printStackTrace();
 				e.getCause(); }
