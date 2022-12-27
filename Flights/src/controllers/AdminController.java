0917f3518a5a1b6e1b881f70c70427f8dfd9be71
aarons <aarons@aaronss-air.attlocal.net>
2022-12-27 01:17:55 -0500

    flight db created // admin can add/delete flights // admins registered as users and admin

--- Flights/src/controllers/AdminController.java
+++ Flights/src/controllers/AdminController.java
@@ -1,8 +1,12 @@
 package controllers;
 
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,31 +15,76 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 public class AdminController {
 	
 	@FXML
-	private Button addButton;
-	private Button deleteButton;
-	private TextField flightId;
-	private TextField capacity;
-	private TextField cityFrom;
-	private TextField cityto;
-	private TextField idToDelete;
-	private DatePicker depart;
-	private DatePicker arrive;
+	private TextField tf_flightid;
+	@FXML
+	private TextField tf_capacity;
+	@FXML
+	private TextField tf_tocity;
+	@FXML
+	private TextField tf_fromcity;
+	@FXML
+	private TextField tf_id_delete;
+
+
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 	
-	public void add (ActionEvent event) throws IOException, SQLException  {
+	public void add (ActionEvent event) throws SQLException  {
+			Connection connection = DriverManager.getConnection
+					("jdbc:sqlserver://javaflightdb.database.windows.net:1433;database=javaflightdb;user=javaflightdb@javaflightdb;password=CISproject22!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+			
+			try {
+			String b = "INSERT INTO Flightts (flightid, capacity, tocity, fromcity) VALUES (?, ?, ?, ?)";
+			PreparedStatement statement = connection.prepareStatement(b);
+		
+			//wont work when i try to get tf from user (look below)
+			
+			statement.setString(1, tf_flightid.getText());
+			statement.setString(2, tf_capacity.getText());
+			statement.setString(3, tf_tocity.getText());
+			statement.setString(4, tf_fromcity.getText());
+	
+			System.out.println("Inserted");
+			
+			statement.executeUpdate(); 
+			
+
+			}catch (Exception e) {
+				e.printStackTrace();
+				e.getCause(); }
+	}
+	public void delete (ActionEvent event) throws SQLException  {
+		Connection connection = DriverManager.getConnection
+				("jdbc:sqlserver://javaflightdb.database.windows.net:1433;database=javaflightdb;user=javaflightdb@javaflightdb;password=CISproject22!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+		
+		try {
+		String b = "DELETE from Flightts where flightid = (?)";
+		PreparedStatement statement = connection.prepareStatement(b);
+		
+		//wont work when i try to get tf from user (look below)
+		
+		statement.setString(1, tf_id_delete.getText());
+	
+
+		System.out.println("Deleted");
+		
+		statement.executeUpdate(); 
+		
+
+		}catch (Exception e) {
+			e.printStackTrace();
+			e.getCause(); }
 		
 	}
-	
-	public void delete (ActionEvent event) throws IOException, SQLException {
 		
-	}
-	
 	public void back (ActionEvent event) throws IOException{
 		Parent root = FXMLLoader.load(getClass().getResource("../gui/Logout.fxml"));
 		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
--- Flights/src/controllers/AdminRegisterController.java
+++ Flights/src/controllers/AdminRegisterController.java
@@ -41,7 +41,9 @@ public class AdminRegisterController {
 		stage.show();
 	}
 //insert info and click submit in oder for it to go to database
-	public void AdminSubmit (ActionEvent event) throws SQLException {
+
+
+	public void addToAdminAccounts (ActionEvent event) throws SQLException {
 		Connection connection = DriverManager.getConnection
 				("jdbc:sqlserver://javaflightdb.database.windows.net:1433;database=javaflightdb;user=javaflightdb@javaflightdb;password=CISproject22!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 		try {
@@ -52,47 +54,51 @@ public class AdminRegisterController {
 		statement.setString(1, tf_username.getText());
 		statement.setString(2, tf_password.getText());
 		statement.setString(3, tf_security_answer.getText());
-		
+
 		System.out.println("inserted into admin accts");
 		statement.executeUpdate();
 		
 			
+
 		//statement.setString(1, "hi");
 		//statement.setString(2, "hello");
 		//statement.setString(3, "bye");
-	
-		}
 		
-		catch (Exception e) {
+		System.out.println("inserted");
+		
+		statement.executeUpdate();
+
+
+
+		}catch (Exception e) {
 			e.printStackTrace();
-			e.getCause();
+			e.getCause(); 
 		}
+			}
 		
-	}
-	public void UserSubmit (ActionEvent event) throws SQLException {
-		Connection connection = DriverManager.getConnection
-				("jdbc:sqlserver://javaflightdb.database.windows.net:1433;database=javaflightdb;user=javaflightdb@javaflightdb;password=CISproject22!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
-		try {
-	String data = "INSERT INTO UserAccounts (username,password,securityanswer) VALUES (?,?,?)";
-	PreparedStatement stat = connection.prepareStatement(data);
-
-	stat.setString(1, tf_username.getText());
-	stat.setString(2, tf_password.getText());
-	stat.setString(3, tf_security_answer.getText());
-	
-	System.out.println("inserted into user accts");
-	stat.executeUpdate();
+	public void addToUserAccounts (ActionEvent event) throws SQLException {
+			Connection connection = DriverManager.getConnection
+					("jdbc:sqlserver://javaflightdb.database.windows.net:1433;database=javaflightdb;user=javaflightdb@javaflightdb;password=CISproject22!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+			try {
+			String data = "INSERT INTO UserAccounts (username,password,securityanswer) VALUES (?,?,?)";
+			PreparedStatement stat = connection.prepareStatement(data);
 		
-	//statement.setString(1, "hi");
-	//statement.setString(2, "hello");
-	//statement.setString(3, "bye");
+			
+			stat.setString(1, tf_username.getText());
+			stat.setString(2, tf_password.getText());
+			stat.setString(3, tf_security_answer.getText());
+		
+			System.out.println("inserted");
+			
+			stat.executeUpdate();
+			
+			
+			}catch (Exception e) {
+				e.printStackTrace();
+				e.getCause();
+			}
+			
+		
 
 	}
-	
-	catch (Exception e) {
-		e.printStackTrace();
-		e.getCause();
 	}
-	}
-}
-
